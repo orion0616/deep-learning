b367b327e92952c0e76fe37bae03fc0b7770c1e5
orion46 <earth.nobu.light@gmail.com>
2017-06-06 18:30:21 +0900

    Add chap8

---
+++ chap8/homework.py
@@ -0,0 +1,142 @@
+def homework(train_X, train_y, test_X):
+    import numpy as np
+    import tensorflow as tf
+    import matplotlib.pyplot as plt
+    from sklearn.utils import shuffle
+    from sklearn.metrics import f1_score
+    from sklearn.model_selection import train_test_split
+    from keras.datasets import cifar10
+    rng = np.random.RandomState(1234)
+    random_state = 42
+
+    class BatchNorm:
+        def __init__(self, shape, epsilon=np.float32(1e-5)):
+            self.gamma = tf.Variable(np.ones(shape, dtype='float32'), name='gamma')
+            self.beta  = tf.Variable(np.zeros(shape, dtype='float32'), name='beta')
+            self.epsilon = epsilon
+
+        def f_prop(self, x):
+            if len(x.get_shape()) == 2:
+                mean, var = tf.nn.moments(x, axes=0, keepdims=True)
+                std = tf.sqrt(var + self.epsilon)
+            elif len(x.get_shape()) == 4:
+                mean, var = tf.nn.moments(x, axes=(0,1,2), keep_dims=True)
+                std = tf.sqrt(var + self.epsilon)
+            normalized_x = (x - mean) / std
+            return self.gamma * normalized_x + self.beta
+
+    class Conv:
+        def __init__(self, filter_shape, function=lambda x: x, strides=[1,1,1,1], padding='VALID'):
+            # Xavier
+            fan_in = np.prod(filter_shape[:3])
+            fan_out = np.prod(filter_shape[:2]) * filter_shape[3]
+            self.W = tf.Variable(rng.uniform(
+                            low=-np.sqrt(6/(fan_in + fan_out)),
+                            high=np.sqrt(6/(fan_in + fan_out)),
+                            size=filter_shape
+                        ).astype('float32'), name='W')
+            self.b = tf.Variable(np.zeros((filter_shape[3]), dtype='float32'), name='b') # バイアスはフィルタごと
+            self.function = function
+            self.strides = strides
+            self.padding = padding
+
+        def f_prop(self, x):
+            u = tf.nn.conv2d(x, self.W, strides=self.strides, padding=self.padding) + self.b
+            return self.function(u)
+
+    class Pooling:
+        def __init__(self, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID'):
+            self.ksize = ksize
+            self.strides = strides
+            self.padding = padding
+        
+        def f_prop(self, x):
+            return tf.nn.max_pool(x, ksize=self.ksize, strides=self.strides, padding=self.padding)
+
+    class Flatten:
+        def f_prop(self, x):
+            return tf.reshape(x, (-1, np.prod(x.get_shape().as_list()[1:])))
+
+    class Dense:
+        def __init__(self, in_dim, out_dim, function=lambda x: x):
+            # Xavier
+            self.W = tf.Variable(rng.uniform(
+                            low=-np.sqrt(6/(in_dim + out_dim)),
+                            high=np.sqrt(6/(in_dim + out_dim)),
+                            size=(in_dim, out_dim)
+                        ).astype('float32'), name='W')
+            self.b = tf.Variable(np.zeros([out_dim]).astype('float32'))
+            self.function = function
+
+        def f_prop(self, x):
+            return self.function(tf.matmul(x, self.W) + self.b)
+
+    class Activation:
+        def __init__(self, function=lambda x: x):
+            self.function = function
+        
+        def f_prop(self, x):
+            return self.function(x)
+
+
+    # definition
+    layers = [ # (縦の次元数)x(横の次元数)x(チャネル数)
+        Conv((3, 3, 3, 32)), # 32x32x3 -> 30x30x32
+        BatchNorm((30, 30, 32)),
+        Activation(tf.nn.relu),
+        Pooling((1, 2, 2, 1)), # 30x30x32 -> 15x15x32
+        Conv((3, 3, 32, 64)), # 15x15x32 -> 13x13x64
+        BatchNorm((13, 13, 64)),
+        Pooling(((1, 2, 2, 1))), # 13x13x64 -> 6x6x64
+        Conv((3, 3, 64, 128)), # 6x6x64 -> 4x4x128
+        BatchNorm((4, 4, 128)),
+        Activation(tf.nn.relu),
+        Pooling((1, 2, 2, 1)), # 4x4x128 -> 2x2x128
+        Flatten(),
+        Dense(2*2*128, 256, tf.nn.relu),
+        Dense(256, 10, tf.nn.softmax)
+    ]
+
+    x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    t = tf.placeholder(tf.float32, [None, 10])
+
+    def f_props(layers, x):
+        for layer in layers:
+            x = layer.f_prop(x)
+        return x
+
+    y = f_props(layers, x)
+
+    cost = -tf.reduce_mean(tf.reduce_sum(t * tf.log(tf.clip_by_value(y, 1e-10, 1.0)), axis=1))
+    train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+    valid = tf.argmax(y, 1)
+
+
+    # trainig
+    n_epochs = 10
+    batch_size = 100
+    n_batches = train_X.shape[0]//batch_size
+
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    
+    with tf.Session() as sess:
+        sess.run(init)
+
+        for epoch in range(n_epochs):
+            zca_train_X, zca_train_y = shuffle(zca_train_X, zca_train_y, random_state=random_state)
+            for i in range(n_batches):
+                start = i * batch_size
+                end = start + batch_size
+                sess.run(train, feed_dict={x: zca_train_X[start:end], t: zca_train_y[start:end]})
+            pred_y, valid_cost = sess.run([valid, cost], feed_dict={x: zca_valid_X, t: zca_valid_y})
+            print('EPOCH:: %i, Validation cost: %.3f, Validation F1: %.3f' % (epoch + 1, valid_cost, f1_score(np.argmax(valid_y, 1).astype('int32'), pred_y, average='macro')))
+
+        # TODO: rewrite z!
+        z = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        result = f_props(layers, z)
+        ans = tf.argmax(result, 1)
+        pred_y = sess.run(ans, feed_dict={z: test_X})
+
+    return pred_y
